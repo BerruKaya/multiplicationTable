e73d8eec17e144aa4362ea149753501a9d4598ea
Berru Kaya <125195172+BerruKaya@users.noreply.github.com>
2023-08-19 13:53:58 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+      //creating a multiplication table by using nested for loops
+      
+      for(int i =1; i<=10;i++){
+          for(int j = 1;j<=10; j++){
+              System.out.println(i + " times " + j + " equals " + (i*j));
+          }
+          System.out.println("****");
+      }
+          }
+
+  }
